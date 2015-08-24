2fd3186e578c4871d0615130ea4d43e75940e226
eashwar5119 <eashwarkumard@gmail.com>
2015-08-23 20:11:46 -0400

    Update appointment.rb

--- assign0818/app/models/appointment.rb
+++ assign0818/app/models/appointment.rb
@@ -2,5 +2,7 @@ class Appointment < ActiveRecord::Base
   belongs_to :doctor
   belongs_to :pet_record
   validates :date_of_visit , {presence: true}
-  validates :customer , {presence: true}
+  validates :customer 
+  
+  
 end
